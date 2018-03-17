ba14ea1343cbda29def803d9dede4bf496479b93
DELL-PC\DELL <fernsutasa@gmail.com>
2018-03-17 17:31:23 +0700

    push file

---
+++ app/ai.php
@@ -0,0 +1,47 @@
+<?php
+
+class AI
+{
+    public static function process($text)
+    {
+        $result = [
+            'gender' => self::getGender($text),
+            'sentiment' => self::getSentiment($text),
+            'rude_words' => self::getRudeWords($text),
+            'languages' => self::getLanguages($text),
+        ];
+        return $result;
+    }
+
+    /**
+     * @return string 'Male' or 'Female' or 'Unknown'
+     */
+    public static function getGender($text)
+    {
+        return 'Male';
+    }
+
+    /**
+     * @return string 'Positive' or 'Neutral' or 'Negative'
+     */
+    public static function getSentiment($text)
+    {
+        return 'Neutral';
+    }
+
+    /**
+     * @return array of all rude words in Thai
+     */
+    public static function getRudeWords($text)
+    {
+        return ['แสส'];
+    }
+
+    /**
+     * @return array of languages (TH, EN)
+     */
+    public static function getLanguages($text)
+    {
+        return ['TH', 'EN'];
+    }
+}
